2f7b2763eb357fdbfb66f8d03a752589a31ad234
Stephen Woosley <stephenrwoosley@gmail.com>
2018-01-16 19:17:36 -0500

    adding app.js functionality

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 $( () => {
 
+  const apiURL = "http://api.alperg.com/forms";
 
-  
-})
+  function requestData() {
+    $.ajax({
+      url: apiURL,
+      method: "GET"
+    })
+    .done( (response) => {
+      console.log(response);
+      renderTableBody(response);
+      
+    });
+  }
+
+  function renderTableBody(formData) {
+    let table = $("#forms-table");
+    let tbody = $("<tbody>");
+
+    $.each(response, (index, form) => {
+      let tr = $("<tr>");
+      let idTd = $("<td>");
+      idTd.text(form.formId);
+
+      let nameTd = $("<td>");
+      nameTd.text(form.formName);
+
+      let btnTd = $("<td>");
+      let btnRender = $("<button>").text("Render").addClass("render-button");
+
+      btnTd.append(btnRender);
+
+      tr.append(idTd, nameTd, btnTd);
+      tbody.append(tr);
+    });
+    table.append(tbody);
+  }
+
+  requestData();
+
+});
